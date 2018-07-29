eb8a9722fb44494d69b732a0efbe1f2d9759678a
77Vincent <wentianqi77@outlook.com>
2018-07-29 09:50:10 +0800

    rate limit

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const convert = require('koa-convert')
 const logger = require('koa-logger')
 const cors = require('koa-cors')
 const bodyParser = require('koa-bodyparser')
+const limit = require('koa-better-ratelimit')
 
 const config = require('./config')
 const routes = require('./routes')
@@ -12,6 +13,10 @@ app.proxy = true
 app.use(convert(logger()))
 app.use(convert(cors()))
 app.use(convert(bodyParser({ jsonLimit: '10mb' })))
+app.use(limit({
+  duration: 1000 * 60 * 5,
+  max: 600,
+}))
 
 app.use(async (ctx, next) => {
   await next()
